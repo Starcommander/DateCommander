ba58ec62989235df1a567117b51a691c9b23eb94
Starcom <soundmodul@gmx.at>
2019-07-21 17:38:29 +0200

    Builder of installer for debian included.

---
+++ installer/debian/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+APP_DIR_RT=./data/usr/share/tomcat8-dater
+APP_DIR=$APP_DIR_RT/dater
+APP_SRC=../../war/
+
+do_check()
+{
+  if [ ! -d $1 ]; then
+    echo "Error, missing directory: $1"
+    exit 1
+  fi
+}
+
+do_clean()
+{
+  if [ -d $APP_DIR ]; then
+    rm -r -f $APP_DIR
+  fi
+}
+
+do_check $APP_SRC
+do_check $APP_DIR_RT
+do_check ./target
+
+do_clean
+cp -r $APP_SRC $APP_DIR
+dpkg-deb -b data/ target/date-commander.deb
