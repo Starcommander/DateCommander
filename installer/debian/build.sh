bd816d9c82b052c6aa380ddbb3e41545b4523e34
001121578 <paul.kashofer@engel.at>
2023-04-21 12:11:09 +0200

    Update debian build to set version and hide from git changes.

--- installer/debian/build.sh
+++ installer/debian/build.sh
@@ -3,6 +3,8 @@
 APP_DIR_RT=./data/usr/share/tomcat9-dater
 APP_DIR=$APP_DIR_RT/dater
 APP_SRC=../../war/
+APP_VERS=X.X-master
+CTL_FILE=data/DEBIAN/control
 
 do_check()
 {
@@ -20,11 +22,37 @@ do_clean()
   fi
 }
 
+get_vers()
+{
+  if type git ; then
+    APP_VERS=1.$(git log | grep -c "^commit [0-9a-f]")-$(git branch --show-current)
+  else
+    APP_VERS=$(cat $CTL_FILE | grep "^Version: " | cut -d' ' -s -f 2)
+  fi
+}
+
+set_vers()
+{
+  sed -i -e "s/^Version:.*/Version: $APP_VERS/g" $CTL_FILE
+}
+
+reset_vers()
+{
+  if type git ; then
+    git checkout data/DEBIAN/control
+  fi
+}
+
 do_check $APP_SRC
 do_check $APP_DIR_RT
+get_vers
 
 do_clean
 cp -r $APP_SRC $APP_DIR
-fakeroot dpkg-deb -b data/ date-commander.deb
+rm -f $APP_DIR/*.war # WebArchive not necessary.
+set_vers
+mkdir -p target
+fakeroot dpkg-deb -b data/ target/date-commander_v${APP_VERS}.deb
+reset_vers
 do_clean
 echo "Finish."
